5ad5ea17c1a0c1f481de75bc4bae28d664ddc730
903345072 <903345072@qq.com>
2019-12-05 18:17:06 +0800

    dassad

---
+++ src/components/pages/index/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <div class="header">
+       <div class="header-img">
+           <img :src="img" alt="">
+       </div>
+       <div class="user-info"></div>
+       <div class="act"></div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      img: "http://localhost:8081/static/123.png"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .header{
+        width: 100%;
+        height: 2rem;
+        display: flex;
+    }
+    .header-img{
+        width: 20%;
+    }
+</style>
---
+++ src/components/pages/index/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+
+export default {
+  name: 'Index',
+  components: {
+      Header
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
